4831e13b36e9567f3c8c5c11841a189227fe3682
MOHAMMADADE-albohisi <mohammadalbohisi2@gmail.com>
2022-11-14 19:40:35 +0200

    EVENT

--- resources/views/cms/eventss/index.blade.php
+++ resources/views/cms/eventss/index.blade.php
@@ -18,7 +18,7 @@
             <span class="card-label font-weight-bolder text-dark">{{__('cms.events')}}</span>
             <span class="text-muted mt-3 font-weight-bold font-size-sm"></span>
         </h3>
-        @can('Create-Event')
+        @can('Create_Event')
         <div class="card-toolbar">
             <a href="{{route('events.create')}}"
                 class="btn btn-info font-weight-bolder font-size-sm">{{__('cms.create')}}</a>
